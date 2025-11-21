6e8c4d871113b419003fd04a63f7343e6aeb52a0
bhagyashreemanaguli <managulibhagyashri@gmail.com>
2025-11-21 10:58:14 +0530

    Create malloc.c

---
+++ malloc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+    float *p = (float*)calloc(4,sizeof(float));
+    p[0] = 7.248;
+    p[1] = 90;
+    p[2] = 100;
+    p[3] = 125;
+    printf("%f ",p[0]); 
+}
